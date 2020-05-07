e818d4ce63ea2e19bf94118a2fd8aa9dfb10ae1b
Yoric Mangeart <yoricm@free.fr>
2020-05-07 10:27:11 +0200

    Add different solutions

--- eloquent_javascript/chapter02/ex01/index.js
+++ eloquent_javascript/chapter02/ex01/index.js
@@ -5,9 +5,17 @@ for(let i = 0; i<7; i++) {
   line += "#";
 }
 
+// Or we can write it shorter:
+for(let i=0, line= "#"; i<7; i++, line += "#")
+  console.log(line);
+
 // Another solution
 let line2 = "#"
 while(line2.length <= 7) {
   console.log(line2);
   line2 += "#";
 }
+
+// shortest version
+for(let lin = "#"; lin.length <= 7; lin += "#")
+  console.log(lin);
