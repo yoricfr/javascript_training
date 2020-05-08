5cc8c16d6d78f98565451c25ad9140b67c5fef77
Yoric Mangeart <yoricm@free.fr>
2020-05-08 20:24:41 +0200

    Optimization

--- eloquent_javascript/chapter04/ex03/index.js
+++ eloquent_javascript/chapter04/ex03/index.js
@@ -4,12 +4,8 @@
 
 let arrayToList = array => {
   let queue = null;
-  let old = null;
   for (let i = array.length - 1; i >= 0; i--) {
-    queue = {}
-    queue["value"] = array[i];
-    queue["rest"] = old;
-    old = queue;
+    queue = { value: array[i], rest: queue }
   }
   return queue;
 }
@@ -24,19 +20,22 @@ let listToArray = list => {
 }
 
 let prepend = (e, list) => {
-  obj = {};
-  obj["value"] = e;
-  obj["rest"] = list;
-  return obj;
+  return {value: e, rest: list};
 }
 
-let nth = (list, n) => {
+let nth1 = (list, n) => {
   let i = 0;
   while (i < n && list) {
     list = list.rest;
     i++;
   }
-  return list.value;
+  return list ? list.value : undefined;
+}
+
+let nth = (list, n) => {
+  if (!list) return undefined;
+  if (n == 0) return list.value;
+  else return nth(list.rest, n-1);
 }
 
 console.log(arrayToList([10, 20]));
@@ -45,6 +44,6 @@ console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
 console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
-console.log(nth(arrayToList([10, 20, 30]), 1));
+console.log(nth1(arrayToList([10, 20, 30]), 81));
 // → 20
 
